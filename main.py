97cf2a79d96725f69b67eb121e9acd25d014eb4b
vrazpopov <72231314+vrazpopov@users.noreply.github.com>
2021-02-27 14:25:56 -0500

    Revert "long/db"
    This reverts commit b1ffd474d38be12f23c1f99b3fec84691a8ce3e9.

--- main.py
+++
@@ -1,7 +0,0 @@
-# this is the main file that will start the application
-
-from login import *
-from database import start_db
-
-start_db()
-start_login()
--- menu.py
+++
@@ -1,2 +0,0 @@
-# This file contains the the guts of the main screen.
-# From here the user can direct themselves throughout the application
